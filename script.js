e0abd6eeedb2ffaafc059bbd2290e613fcc107be
D-A-V-I-D-A-R-A-N-G-O <davidpeewee01241@outlook.com>
2025-02-15 22:20:08 -0500

    primera

---
+++ script.js
@@ -0,0 +1,35 @@
+async function obtenerPersonajes() {
+    try {
+        const response = await fetch('https://api.batmanapi.com/v1/characters/');
+        const datas = await response.json();
+        const avesContainer = document.getElementById('Personajes');
+        console.log(datas.data); 
+        datas.data.forEach(ave => {
+            const aveElement = document.createElement('div');
+            aveElement.classList.add('ave');
+            aveElement.innerHTML = 
+            `<div class="personaje">
+                <h2 id="alias">${ave.attributes.alias}</h2>
+                <h2 id="nombre">${ave.attributes.name}</h2>
+                <h3>ROL: ${ave.attributes.role}</h3>
+            </div>`;
+            avesContainer.appendChild(aveElement);
+        });
+    } catch (error) {
+        console.error("no se pudo hacer", error);
+    }
+}
+window.onload = obtenerPersonajes();
+
+function personajesBoton() {
+    const personajes = document.getElementById('Personajes');
+    const inicio = document.getElementById('Inicio');
+    personajes.style.display = 'block';
+    inicio.style.display = 'none';
+}
+function inicioBoton() {
+    const personajes = document.getElementById('Personajes');
+    const inicio = document.getElementById('Inicio');
+    personajes.style.display = 'none';
+    inicio.style.display = 'block';
+}
