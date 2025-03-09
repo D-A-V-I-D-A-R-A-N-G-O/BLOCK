b04b5a45dea109b0815b1727c5d63a5d47a95f0a
D-A-V-I-D-A-R-A-N-G-O <davidpeewee01241@outlook.com>
2025-03-08 21:01:54 -0500

    nada

--- js/10.js
+++ js/10.js
@@ -1,12 +1,8 @@
 async function obtenerPersonajes() {
     try {
-        const response = await fetch('https://api.batmanapi.com/v1/characters/');
+        const response = await fetch('http://gia.com.co:8080/empresarial');
         const datas = await response.json();
-        const personajes = datas.data;
-        
-        // Filtrar personajes con ID <= 30
-        const pepe = personajes.filter(caracter => caracter.id <= 20);
-        console.log(pepe);
+        console.log(datas);
     } catch (error) {
         console.error("Error al obtener personajes:", error);
     }
